6084685508642e61a45f1d6b938d688d44a28276
Haikal Iskandar <104144864+Haikalwelp@users.noreply.github.com>
2024-01-20 09:35:44 +0800

    v 0.5.1
    Theming fixes.

--- app/src/main/java/com/dreamshape/dsfitness/DSFActivity.kt
+++ app/src/main/java/com/dreamshape/dsfitness/DSFActivity.kt
@@ -1,18 +1,25 @@
 package com.dreamshape.dsfitness
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.dreamshape.dsfitness.ui.theme.DSFitnessTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DSFitnessTheme {
+            MaterialTheme(
+                colors = lightColors() // This applies the default light color scheme
+            ) {
                 // Create a NavController
                 val navController = rememberNavController()
 
